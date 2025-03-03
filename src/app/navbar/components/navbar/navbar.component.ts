7147f60553627858846031e66eebca6ff74d0f00
Daniel Volk <mail@volkarts.com>
2025-03-03 19:05:55 +0100

    Use routerLink instead of href

--- src/app/navbar/components/navbar/navbar.component.ts
+++ src/app/navbar/components/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavLinkDirective } from '../../directives/navlink.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     imports: [
         NavLinkDirective,
+        RouterLink,
     ],
     templateUrl: './navbar.component.html',
     styles: ``
--- src/app/page-home/components/page-home/page-home.component.ts
+++ src/app/page-home/components/page-home/page-home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-page-home',
     standalone: true,
-    imports: [],
+    imports: [
+        RouterLink,
+    ],
     templateUrl: './page-home.component.html',
     styles: ``
 })
